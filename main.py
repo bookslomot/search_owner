bab3d4f2ddf34cf971610327d59ece3b0f33d3cd
bookslomot <alexgol2101@gmail.com>
2022-05-08 22:04:14 +0400

    fix_bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import zipfile
 
 import win32security
 from os.path import isfile, join
@@ -12,13 +12,33 @@ def create_dict_f():
     for i in range(len(list_text)):
         if list_text[i].split('.')[1] in ('xlsx', 'xls', 'doc', 'ppt', 'pptx', 'docx'):
             path_doc = path + '\\' + list_text[i]
-            sd = win32security.GetFileSecurity(path_doc, win32security.OWNER_SECURITY_INFORMATION)
-            owner_sid = sd.GetSecurityDescriptorOwner()
-            name, domain, type = win32security.LookupAccountSid(None, owner_sid)
             try:
-                name_dict[name] += [list_text[i]]
-            except KeyError:
-                name_dict[name] = [list_text[i]]
+                print('fewfewfefwef' + 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
+                z = zipfile.ZipFile(path_doc, 'r')
+                z.printdir()
+                z.extract('docProps/core.xml')
+                with open('docProps/core.xml', 'r') as f:
+                    string = f.readlines()[1]
+                    print(string)
+                    end = string.find('</dc:creator>')
+                    start = string.find('<dc:creator>')
+                    print(start)
+                    print(end)
+                    print('------------------    ' + string[start + 12:end] + '    ---------------')
+                name = string[start + 12:end]
+                try:
+                    name_dict[name] += [list_text[i]]
+                except KeyError:
+                    name_dict[name] = [list_text[i]]
+
+            except:
+                sd = win32security.GetFileSecurity(path_doc, win32security.OWNER_SECURITY_INFORMATION)
+                owner_sid = sd.GetSecurityDescriptorOwner()
+                name, domain, type = win32security.LookupAccountSid(None, owner_sid)
+                try:
+                    name_dict[name] += [list_text[i]]
+                except KeyError:
+                    name_dict[name] = [list_text[i]]
 
     return path, name_dict
 
@@ -36,7 +56,11 @@ def write_in_owner(path, text_owner):
 def main():
     path, name_dict = create_dict_f()
     write_in_owner(path, name_dict)
+    print('ПУК')
 
 
 if __name__ == '__main__':
     main()
+
+
+
