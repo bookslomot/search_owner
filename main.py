c4a4ab7da229bf92aa3ad220732f1e82c36f5f22
bookslomot <alexgol2101@gmail.com>
2022-05-08 18:09:53 +0400

    fix

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def write_in_owner(path, text_owner):
     with open(path + '\\' + 'owner_doc.txt', 'w') as f:
         for i in range(len(text_owner)):
             string_name = str(text_owner)
-            f.write(f"{str(string_name).split(':')[0]} + '\n' + {str(string_name).split(':')[1]}")
+            f.write(f"{string_name.split(':')[0]} + '\n' + {string_name.split(':')[1]}")
 
 
 def main():
